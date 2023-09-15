7b6ab73f196c15ba72fe5af2d2751ba4e7c75b55
Саша Касаткин <virusgamers123231@gmail.com>
2023-09-15 14:50:47 +0500

    добавлена практика 2

---
+++ PZ_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace PZ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число y");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число x");
+            double x = double.Parse(Console.ReadLine());
+            double s;
+            double t;
+            double v;
+
+            if (y < 2)
+
+                s = y - y * Math.Pow(x, 2) / (x + 1);
+            else
+                s = -10.6 * x * y;
+            if (s > 0)
+                t = s - Math.Pow(Math.Cos(s/x), 2);
+            else
+                t = y * s + Math.Sin(x) + y;
+
+            v = 2 * y * x + 3 * y * s - s * t;
+            Console.WriteLine(v);
+        }  
+    }
+}
