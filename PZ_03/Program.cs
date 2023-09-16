d18155ab260146e4d497363c595db1e06668589d
Саша Касаткин <virusgamers123231@gmail.com>
2023-09-16 10:45:48 +0500

    добавлена практика 3

---
+++ PZ_03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Висит груша нельзя скушать. Что это?");
+         
+                var a = Console.ReadLine();
+
+                switch (a)
+                {
+                    case "сдаюсь":
+                        Console.WriteLine("Правильный ответ: лампочка");
+                        break;
+                    case "лампочка":
+                        Console.WriteLine("Правильно");
+                        return;
+                    default:
+                        Console.WriteLine("Ответ не верный");
+                    break;
+
+
+                }
+        }
+    }
+}
