e22dabea823207d74c2170a2d193447d6f81d8af
Саша Касаткин <virusgamers123231@gmail.com>
2023-11-23 13:15:40 +0500

    добавлена практика 10

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Произвольный текст ыравыоар выщары флы лы л. для сортировки предложений по. количеству слов.";
+
+            // Разделение текста на предложения
+            string[] sentences = text.Split('.');
+
+            // Удаление пустых предложений
+            sentences = sentences.Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+            // Сортировка предложений по количеству слов
+            Array.Sort(sentences, (a, b) => a.Split(' ').Length.CompareTo(b.Split(' ').Length));
+
+            // Вывод отсортированных предложений
+            foreach (string sentence in sentences)
+            {
+                Console.WriteLine(sentence.Trim());
+            }
+        }
+    }
+}
---
+++ PZ_10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //вводим текст
+            string text = Console.ReadLine();
+            //делим текст на предложения
+            string[] predlojenie = text.Split( ". ");
+            //создание массива
+            string[][] words = new string[predlojenie.Length][];
+
+            for (int i = 0; i < predlojenie.Length; i++)
+            {
+                //разделение строки на слова
+                string[] predlojWords = predlojenie[i].Split(' ');
+                words[i] = predlojWords;
+            }
+
+            Console.WriteLine("Двумерный массив слов:");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write($"Строка {i + 1}: ");
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    Console.Write(words[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+    
