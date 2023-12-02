2db4ef51eccfe775457c8188b78fb83ac2e0b081
Саша Касаткин <virusgamers123231@gmail.com>
2023-12-02 16:57:24 +0500

    добавлена практика 11

---
+++ PZ_11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга:");
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            // Вычисляем площадь круга
+            double S_circle = Math.Pow(radius, 2) * Math.PI;
+            Console.WriteLine($"Площадь круга равна {S_circle}");
+
+            // Вычисляем объем и площадь сферы
+            double obem = Math.Pow(radius, 3) * (4 / 3) * Math.PI;
+            double S_spheri = 4 * Math.Pow(radius, 2) * Math.PI;
+
+            Console.WriteLine($"Объем сферы  равен {obem}");
+            Console.WriteLine($"Площадь сферы равна {S_spheri}");
+
+        }
+    }
+}
