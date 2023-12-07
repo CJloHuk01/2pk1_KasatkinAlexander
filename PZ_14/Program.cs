e4e56a385c6b0bb495038c596ebd34523c5aed5b
Саша Касаткин <virusgamers123231@gmail.com>
2023-12-07 19:11:54 +0500

    добавлена практика 14

---
+++ PZ_14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+namespace PZ_14
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создание перепенных для работы с ними
+            string file1 = "f1.txt";
+            string file2 = "f2.txt";
+            //прочтение первого файла
+            using (StreamReader reader = new StreamReader(file1))
+            //измененение вотрого файла с помощью while и if
+            using (StreamWriter writer = new StreamWriter(file2))
+            {
+                
+                string line;
+                int lineNumber = 1;
+                //пока строка не пустая, то есть 0, выполняется цикл
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //четность строки
+                    if (lineNumber % 2 == 0)
+                    {
+                        writer.WriteLine(line);
+                    }
+
+                    lineNumber++;
+                }
+            }
+
+            Console.WriteLine("Четные строки из файла f1 записаны в файл f2.");
+        }
+    }
+    
+}
---
+++ PZ_15/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полный путь к каталогу:");
+            string path = Console.ReadLine();
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            DirectoryInfo[] subDirs = dirInfo.GetDirectories();
+            Array.Sort(subDirs, (x, y) =>
+            {
+                long xSize = x.GetFiles(".", SearchOption.AllDirectories).Length;
+                long ySize = y.GetFiles(".", SearchOption.AllDirectories).Length;
+                return (xSize > ySize) ? -1 : 1;
+            });
+            for (int i = 0; i < subDirs.Length; i++)
+            {
+                Console.WriteLine(subDirs[i].Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
